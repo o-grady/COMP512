d07ebced1937c78f8fcdeb01d26d38a613c7671a
David Zhou <davidzhou94@gmail.com>
2015-09-28 19:32:03 -0400

    Client-side descriptor classes for communication. Still needs to be implemented server-side.

---
+++ src/shared/RequestDescriptor.java
@@ -0,0 +1,25 @@
+package shared;
+
+import java.io.Serializable;
+
+public class RequestDescriptor implements Serializable {
+
+	private static final long serialVersionUID = -1943991785815079042L;
+	public RequestType requestType;
+	
+    public int id;
+    public int flightNumber;
+    public int flightPrice;
+    public int numSeats;
+    public boolean room;
+    public boolean car;
+    public int price;
+    public int numRooms;
+    public int numCars;
+    public String location;
+    public int customerNumber;
+	
+	public RequestDescriptor(RequestType requestType) {
+		this.requestType = requestType;
+	}
+}
---
+++ src/shared/ResponseDescriptor.java
@@ -0,0 +1,14 @@
+package shared;
+
+import java.io.Serializable;
+
+public class ResponseDescriptor implements Serializable {
+
+	private static final long serialVersionUID = 8637699970545623702L;
+	public String message;
+	
+	public ResponseDescriptor(String response) {
+		this.message = response;
+	}
+
+}
