51a39ce34fd89a8adf8a07acb7a4a9003c8f3c7c
Grady Weber <grady.weber@gmail.com>
2015-10-01 00:24:56 -0400

    webserver is now a functioning static file server, need to build response object on webserver still

--- src/webserver/WebServer.java
+++ src/webserver/WebServer.java
@@ -9,6 +9,7 @@ import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -19,17 +20,44 @@ public class WebServer {
     	System.out.println(System.getProperty("os.name"));
 
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/", new HTMLHandler());
+        server.createContext("/", new StaticFileHandler());
         server.createContext("/request", new RequestHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
 
-    static class HTMLHandler implements HttpHandler {
+    static class StaticFileHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-        	String path = System.getProperty("user.dir")+File.separator+"src"+File.separator+"webserver"+File.separator+"view.html";
-            byte[] response = Files.readAllBytes(Paths.get(path));
+        	System.out.println(t.getRequestURI().toString());
+        	String dirPath = System.getProperty("user.dir")+File.separator+"src"+File.separator+"webserver"+File.separator+"public"+File.separator;
+        	String item = t.getRequestURI().toString().substring(1);
+        	String pathStr;
+        	if(item.equals("")){
+        		pathStr = dirPath + "index.html";
+        	}else{
+        		pathStr = dirPath + item;
+        	}
+        	File f = new File(pathStr);
+        	if(!f.exists() || f.isDirectory()) { 
+        		return;
+        	}
+            byte[] response = Files.readAllBytes(Paths.get(pathStr));
+            int index = item.lastIndexOf(".");
+            String extension = "";
+            if(index >= 0){
+            	extension = item.substring(index);
+            }
+            String contentType;
+            if(extension.equals(".css")){
+            	contentType = "text/css";
+            }else if(extension.equals(".js")){
+            	contentType = "application/javascript";
+            }else{
+            	contentType = "text/html";
+            }
+            Headers h = t.getResponseHeaders();
+            h.set("Content-Type", contentType);
             t.sendResponseHeaders(200, response.length);
             OutputStream os = t.getResponseBody();
             os.write(response);
@@ -40,7 +68,7 @@ public class WebServer {
         @Override
         public void handle(HttpExchange t) throws IOException {
         	InputStream body = t.getRequestBody();
-        	String bodyAsString = convertStreamToString(body).replace("%2C", ",");
+        	String bodyAsString = convertStreamToString(body);//.replace("%2C", ",");
         	System.out.println(bodyAsString);
         }
     }
